19bd5a8e0ff916716d7ec37aaed140d641366229
Rafael Scheffer <rafaelscheff@gmail.com>
2019-11-25 18:56:35 -0300

    Bump version to v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fields.MixProject do
   def project do
     [
       app: :fields,
-      version: "0.1.7",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
