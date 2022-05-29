0de41b5edeae48356a94ff7c268584efddf7a927
João Otávio Biondo <joao.otb@gmail.com>
2022-05-29 12:01:00 -0300

    Use new OTP crypto API and remove deprecated block_encrypt

--- lib/aes.ex
+++ lib/aes.ex
@@ -4,21 +4,26 @@ defmodule Fields.AES do
   https://en.wikipedia.org/wiki/Galois/Counter_Mode
   using a random Initialisation Vector for each encryption,
   this makes "bruteforce" decryption much more difficult.
+
   See `encrypt/1` and `decrypt/1` for more details.
   """
+
   # Use AES 256 Bit Keys for AES.
   @aad "AES256GCM"
 
   @doc """
   Uses a random IV for each call, and prepends the IV and Tag to the
-  ciphertext.  This means that `encrypt/1` will never return the same ciphertext
+  ciphertext. This means that `encrypt/1` will never return the same ciphertext
   for the same value. This makes "cracking" (bruteforce decryption) much harder!
+
   ## Parameters
   - `plaintext`: Accepts any data type as all values are converted to a String
-    using `to_string` before encryption.
+    using `to_string/1` before encryption.
+
   ## Examples
       iex> Fields.AES.encrypt("tea") != Fields.AES.encrypt("tea")
       true
+
       iex> ciphertext = Fields.AES.encrypt(123)
       iex> is_binary(ciphertext)
       true
@@ -26,26 +31,27 @@ defmodule Fields.AES do
   @spec encrypt(any) :: String.t
   def encrypt(plaintext) do
     iv = :crypto.strong_rand_bytes(16) # create random Initialisation Vector
-    {ciphertext, tag} = :crypto.block_encrypt(:aes_gcm, get_key(), iv, {@aad, to_string(plaintext), 16})
+    {ciphertext, tag} = :crypto.crypto_one_time_aead(:aes_256_gcm, get_key(), iv, to_string(plaintext), @aad, true)
     iv <> tag <> ciphertext # "return" iv with the cipher tag & ciphertext
   end
 
   @doc """
   Decrypt a binary using GCM.
+
   ## Parameters
   - `ciphertext`: a binary to decrypt, assuming that the first 16 bytes of the
     binary are the IV to use for decryption.
-  ## Example
+
+  ## Examples
       iex> Fields.AES.encrypt("test") |> Fields.AES.decrypt()
       "test"
   """
   @spec decrypt(any) :: String.t
   def decrypt(ciphertext) do
     <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
-    :crypto.block_decrypt(:aes_gcm, get_key(), iv, {@aad, ciphertext, tag})
+    :crypto.crypto_one_time_aead(:aes_256_gcm, get_key(), iv, ciphertext, @aad, tag, false)
   end
 
-  @spec get_key() :: String
   defp get_key do
     Application.get_env(:fields, Fields.AES)[:key]
   end
